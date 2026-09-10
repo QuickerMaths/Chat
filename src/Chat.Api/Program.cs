03b5c77f3e933d99503cd555ad96c896b3099d28
Mateusz Szałowicz <116837090+QuickerMaths@users.noreply.github.com>
2026-09-10 11:39:22 +0200

    fix(api): use access_token query key for hub JWT authentication

--- src/Chat.Api/Program.cs
+++ src/Chat.Api/Program.cs
@@ -59,7 +59,7 @@ builder.Services
         {
             OnMessageReceived = context =>
             {
-                var token = context.Request.Query["access-token"];
+                var token = context.Request.Query["access_token"];
                 if (!string.IsNullOrEmpty(token) && context.HttpContext.Request.Path.StartsWithSegments("/hubs"))
                     context.Token = token;
 
